3faf0824420f9b19b3c19394769df38ef844501e
AhmadJaenal <ahmadjaenal629@gmail.com>
2024-01-28 21:20:21 +0700

    [+] Add border radius

--- resources/views/landingpage/detail_food.blade.php
+++ resources/views/landingpage/detail_food.blade.php
@@ -92,7 +92,7 @@
         <!-- end slider section -->
     </div>
 
-        <section class="news_section">
+    <section class="news_section">
         <div class="container">
             <div class="heading_container heading_center mb-5">
                 <h2>
@@ -105,121 +105,121 @@
                 <!-- Left Side - Image -->
                 <div class="col-md-6">
                     <div class="img-box">
-                        <img src="{{ $detailRecipe['image'] }}" class="box-img" alt="" style="width: 100%;">
-                        {{-- <img src="https://spoonacular.com/recipeImages/632426-556x370.jpg" class="box-img" alt="" style="width: 100%;"> --}}
+                        <img src="{{ $detailRecipe['image'] }}" class="box-img" alt=""
+                            style="width: 100%; border-radius:30px;">
                     </div>
                 </div>
-            
+
                 <!-- Right Side - Table -->
                 <div class="col-md-6">
                     <div class="container mt-4">
                         <table class="table table-sm">
                             <thead>
                                 <tr>
-                                <th>Property</th>
-                                <th>Status</th>
+                                    <th>Property</th>
+                                    <th>Status</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                <td>Vegetarian</td>
-                                <td>
-                                    @if ($detailRecipe['vegetarian'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Vegetarian</td>
+                                    <td>
+                                        @if ($detailRecipe['vegetarian'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Vegan</td>
-                                <td>
-                                    @if ($detailRecipe['vegan'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Vegan</td>
+                                    <td>
+                                        @if ($detailRecipe['vegan'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Gluten-Free</td>
-                                <td>
-                                    @if ($detailRecipe['glutenFree'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Gluten-Free</td>
+                                    <td>
+                                        @if ($detailRecipe['glutenFree'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Dairy-Free</td>
-                                <td>
-                                    @if ($detailRecipe['dairyFree'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Dairy-Free</td>
+                                    <td>
+                                        @if ($detailRecipe['dairyFree'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Very Healthy</td>
-                                <td>
-                                    @if ($detailRecipe['veryHealthy'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Very Healthy</td>
+                                    <td>
+                                        @if ($detailRecipe['veryHealthy'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Cheap</td>
-                                <td>
-                                    @if ($detailRecipe['cheap'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Cheap</td>
+                                    <td>
+                                        @if ($detailRecipe['cheap'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Very Popular</td>
-                                <td>
-                                    @if ($detailRecipe['veryPopular'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Very Popular</td>
+                                    <td>
+                                        @if ($detailRecipe['veryPopular'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Sustainable</td>
-                                <td>
-                                    @if ($detailRecipe['sustainable'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
-                                </td>
+                                    <td>Sustainable</td>
+                                    <td>
+                                        @if ($detailRecipe['sustainable'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
+                                    </td>
                                 </tr>
                                 <tr>
-                                <td>Low Fodmap</td>
-                                <td>
-                                    @if ($detailRecipe['lowFodmap'])
-                                        <span class="text-success">&#10004;</span>
-                                    @else
-                                        <span class="text-danger">&#10008;</span>
-                                    @endif
+                                    <td>Low Fodmap</td>
+                                    <td>
+                                        @if ($detailRecipe['lowFodmap'])
+                                            <span class="text-success">&#10004;</span>
+                                        @else
+                                            <span class="text-danger">&#10008;</span>
+                                        @endif
 
-                                </td>
+                                    </td>
                                 </tr>
                             </tbody>
-                            </table>
+                        </table>
                     </div>
                 </div>
             </div>
-            
-            
-              
+
+
+
         </div>
     </section>
 
@@ -316,4 +316,5 @@
 
 
 </body>
+
 </html>
