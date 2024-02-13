1d6f19da59e8ed8d97ef80b0679adb487f175604
StefanSetiadi <stefansetiadi452002@gmail.com>
2024-02-13 20:19:24 +0700

    [+] Update navbar route

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,7 +13,7 @@
         <header class="header_section">
             <div class="container-fluid">
                 <nav class="navbar navbar-expand-lg custom_nav-container">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="{{route('featchRecipes')}}">
                         <span>
                             Delfood
                         </span>
